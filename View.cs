9801a74736e34247fe2e658f49bda3ccde7e65a5
kamil-g-cc <60813305+kamil-g-cc@users.noreply.github.com>
2020-03-21 16:21:05 +0100

    Update View.cs
    //just szkolenie

--- View.cs
+++ View.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 
 namespace Iterator {
+    //FIXME: (ignore) please implement missing method (name of a method)
     abstract class View {
         public void Print(string sampleString) { System.Console.WriteLine(sampleString); }
         public void Print(List<string> sampleList) { System.Console.WriteLine(sampleList); }
+        //FIXME: (ignore) the below method should heave 2 arguments 
         public void Print(int sampleInt) { System.Console.WriteLine(sampleInt); }
         public void Print(double sampleDouble) { System.Console.WriteLine(sampleDouble); }
         public void Print(Dictionary<string, int> Integer) { System.Console.WriteLine(Integer); }
